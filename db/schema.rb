2f675f76a05806423028738d525dc43730b60edd
Roberto Livi <robertomlivi@outlook.com>
2020-03-27 20:54:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -35,6 +34,7 @@ ActiveRecord::Schema.define(version: 7) do
     t.integer "network_id"
     t.string "day"
     t.string "season"
+    t.string "genre"
   end
 
 end
