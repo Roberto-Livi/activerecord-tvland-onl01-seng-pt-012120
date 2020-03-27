c743766eb0a7877395a77b76b91e702eb2bc1fe7
Roberto Livi <robertomlivi@outlook.com>
2020-03-27 19:30:18 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,6 +4,10 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
   
   def full_name
+   self.actor.first_name + " " + self.actor.last_name
+  end
+  
+  def list_roles
     
   end
 end
